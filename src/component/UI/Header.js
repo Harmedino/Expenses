887d47f5575e3eba64d5d953e63532be8dafd0be
Harmedino <adamsahmed191@gmail.com>
2023-08-17 14:24:52 +0100

    header jax added

--- src/component/UI/Header.js
+++ src/component/UI/Header.js
@@ -1,13 +1,19 @@
+import React from 'react';
+import './Header.css';
 
+const Header = () => {
+  return (
+    <header className="header">
+      <a href="#" className="logo">Logo</a>
+      <nav className="nav">
+        <ul>
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Signup</a></li>
+          <li><button className="logout-btn">Logout</button></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-function Header() {
-    
-
-    return (
-        <div>
-            Header
-        </div>
-    )
-}
-
-export default Header
+export default Header;
