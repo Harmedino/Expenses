dc665dce0ce768ac6429abcc99ed48bf56acb246
Harmedino <adamsahmed191@gmail.com>
2023-08-17 14:26:07 +0100

    headernavigation addedd

--- src/component/UI/Header.js
+++ src/component/UI/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './Header.css';
 
 const Header = () => {
   return (
     <header className="header">
-      <a href="#" className="logo">Logo</a>
+      <Link to="/" className="logo">Logo</Link> {/* Use Link component */}
       <nav className="nav">
         <ul>
-          <li><a href="#">Login</a></li>
-          <li><a href="#">Signup</a></li>
+          <li><Link to="/login">Login</Link></li> {/* Use Link component */}
+          <li><Link to="/signup">Signup</Link></li> {/* Use Link component */}
           <li><button className="logout-btn">Logout</button></li>
         </ul>
       </nav>
