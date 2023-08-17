d6c3963b25e26d875888817b1964c53d99047255
Harmedino <adamsahmed191@gmail.com>
2023-08-17 15:46:12 +0100

    register updated

--- src/component/Auth/Register.js
+++ src/component/Auth/Register.js
@@ -1,83 +1,99 @@
-import React, { useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate, Link } from 'react-router-dom';
 import "./Login.css";
-import { auth } from "../Firebase";
+import { auth } from '../Firebase';
 
 function RegisterPage() {
-    const history = useNavigate();
-    const [isLoading, setIsLoading] = useState(false);
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const handleFormSubmit = async (e) => {
-        e.preventDefault();
-        
-        try {
-            setIsLoading(true);
-            const result = await createUserWithEmailAndPassword(auth, email, password);
-            console.log(result);
-            setIsLoading(false);
-            history('/'); 
-        } catch (error) {
-            setIsLoading(false);
-            console.log(error.message);
-        }
-    };
+  const history = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-    return (
-        <div className="body">
-            <div className="login">
-                <h1>Register</h1>
-                <form>
-                    <div className="form-control">
-                        <label htmlFor="username">Username</label>
-                        <input
-                            id="username"
-                            placeholder="Enter your username"
-                            name="username"
-                            type="text"
-                            required
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-control">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            id="email"
-                            placeholder="johndoe@gmail.com"
-                            name="email"
-                            type="email"
-                            required
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-control">
-                        <label htmlFor="password">Password</label>
-                        <input
-                            id="password"
-                            placeholder="********"
-                            name="password"
-                            type="password"
-                            required
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <button
-                        className="submit-button"
-                        type="submit"
-                        onClick={handleFormSubmit}
-                        disabled={isLoading}
-                    >
-                        {isLoading ? 'Registering...' : 'Register'}
-                    </button>
-                </form>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    if (errorMessage) {
+      const timer = setTimeout(() => {
+        setErrorMessage('');
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [errorMessage]);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+      const result = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(result);
+      setIsLoading(false);
+      history('/');
+    } catch (error) {
+      setIsLoading(false);
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <div className="register-page">
+      <div className="login">
+        <h1>Register</h1>
+        <form>
+          <div className="form-control">
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              placeholder="Enter your username"
+              name="username"
+              type="text"
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              placeholder="johndoe@gmail.com"
+              name="email"
+              type="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              placeholder="********"
+              name="password"
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            className="submit-button"
+            type="submit"
+            onClick={handleFormSubmit}
+            disabled={isLoading}
+          >
+            {isLoading ? 'Registering...' : 'Register'}
+          </button>
+        </form>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        <p className="login-link">
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default RegisterPage;
